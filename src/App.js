bedcb7f874fc8708788766c966bb7f3ce9c24269
Korin92 <carmene8992@gmail.com>
2022-04-24 19:25:52 +0200

    new features

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
 import './App.css'
-
-import { AppBar, Toolbar, Avatar, styled, alpha, InputBase } from '@mui/material'
-import SearchIcon from '@mui/icons-material/Search'
-//import { Link } from 'react-router-dom'
+import ListOfMovies from './components/listOfmovies/component'
+import NavBar from './components/navBar/component'
 
 function App() {
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }))
-
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }))
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }))
   return (
-    <AppBar>
-      <Toolbar>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-        </Search>
-        <Avatar src="/broken-image.jpg" />
-      </Toolbar>
-    </AppBar>
+    <>
+      <NavBar />
+      <ListOfMovies />
+    </>
   )
 }
 
---
+++ src/components/listOfmovies/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+
+export const STCardMovies = styled.div`
+  
+   margin-top: 7%;
+
+   .title{
+    font-size: 2.5rem;
+    font-weight: bold;
+    color: #f6f6fe;
+    margin-bottom: 2%;
+    margin-left: 3%;
+
+   }
+
+    @media (max-width: 950px) {
+        margin-top: 9%;
+    }
+
+    @media (max-width: 750px) {
+        margin-top: 15%;
+    }
+`
